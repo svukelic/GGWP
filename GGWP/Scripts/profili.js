b5ba471ca9ea8b8c4fedfae91937e6a943c7dfa3
ddrazeno <ddrazeno@foi.hr>
2018-03-07 20:01:01 +0100

    Promjena profila i rasporeda

---
+++ GGWP/Scripts/profili.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+    $(".centerImage").click(function () {
+        $(".left_side").transition('fade left');
+    });
+
+    $(".centerImage").click(function () {
+        $(".right_side").transition('fade right');
+    });
+
+    $(".profili_detalji").click(function () {
+        $('.seq .lab')
+            .transition({
+                animation: 'scale',
+                reverse: 'auto',
+                interval: 200
+            })
+            ;
+    });
+
+    $(".profili_detalji").click(function () {
+        $(".profili_raspored").transition('slide down');
+    });
+});
